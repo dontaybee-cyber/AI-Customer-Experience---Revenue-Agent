1fda02f0848d1ac151b11c3092484d92b7954fce
Dontay Beemon <dontaybeemon@gmail.com>
2026-02-22 15:04:47 -0700

    build: added start script and filters for railway deployment 🚀

--- apps/api/dist/types.d.ts
+++
@@ -1,9 +0,0 @@
-export interface Message {
-    id: string;
-    channel: string;
-    identity: string;
-    content: string;
-    timestamp: number;
-    conversationId: string;
-}
-//# sourceMappingURL=types.d.ts.map
